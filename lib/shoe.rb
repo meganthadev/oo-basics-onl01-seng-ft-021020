8bf223e8000cd2ae86010fb6d1726f770222b640
Megan Horsfield <meganthadev@gmail.com>
2020-02-23 07:28:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,8 @@
 class Shoe 
-    attr_accessor :color, :size, :material, :condition
-
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   def initialize(brand)
-   @title = title
-  end
-  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+   @brand = brand
   end
   
 end  
